1b7c3234f09e665db886544558309f6f6ed30365
ashbb <ashbbb@gmail.com>
2012-02-22 23:39:50 +0900

    Added sample 29 and 34.

---
+++ samples/sample29.rb
@@ -0,0 +1,12 @@
+require '../lib/purple_shoes'
+
+Shoes.app do
+  lb = list_box items: COLORS.keys.map(&:to_s), choose: 'red' do |s|
+    @o.style fill: eval(s.text)
+    @p.text = s.text
+  end.move(300, 0)
+  @p = para
+  nostroke
+  @o = oval 100, 100, 100, 100
+  button('print'){para lb.text; flush}.move(500, 0)
+end
