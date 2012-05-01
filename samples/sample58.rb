932ef2da81f5ddfbcdaf84102c625b6edf965bd8
ashbb <ashbbb@gmail.com>
2012-05-01 17:06:04 +0900

    Corrected the path of sample sounds in sample 58.

--- samples/sample58.rb
+++ samples/sample58.rb
@@ -4,18 +4,18 @@ Shoes.app do
   title 'Sample Sounds', align: 'center', margin: 50
 
   button "Boing WAV (740ms)" do
-    video("./sounds/61847__simon-rue__boink-v3.wav").play
+    video(File.join DIR, "../samples/sounds/61847__simon-rue__boink-v3.wav").play
   end
 
   button "Fog Horn AIFF (18.667s)" do
-    video("./sounds/145622__andybrannan__train-fog-horn-long-wyomming.aiff").play
+    video(File.join DIR, "../samples/sounds/145622__andybrannan__train-fog-horn-long-wyomming.aiff").play
   end
 
   button "Explosion MP3 (4.800s)" do
-    video("./sounds/102719__sarge4267__explosion.mp3").play
+    video(File.join DIR, "../samples/sounds/102719__sarge4267__explosion.mp3").play
   end
 
   button "Shields UP! OGG (2.473s)" do
-    video("./sounds/46492__phreaksaccount__shields1.ogg").play
+    video(File.join DIR, "../samples/sounds/46492__phreaksaccount__shields1.ogg").play
   end
 end
